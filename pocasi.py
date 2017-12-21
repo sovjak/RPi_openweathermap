ceb779cca4a8479a85e102b3153564d05242671f
sovjak <34157900+sovjak@users.noreply.github.com>
2017-12-21 13:24:49 +0100

    Update pocasi.py

--- pocasi.py
+++ pocasi.py
@@ -1,4 +1,4 @@
-#http://api.openweathermap.org/data/2.5/weather?q=Ostrava,cz&APPID=be5bc367823a074effb436947b75a397
+#http://api.openweathermap.org/data/2.5/weather?q=Ostrava,cz&APPID=tvujklic
 #You will need to find your particular city ID number and put it in the two URLs Below
 # search for your city by name on this site:
 #http://openweathermap.org/help/city_list.txt
@@ -7,7 +7,7 @@ import urllib2
 import json
 def gettemp():
     """ call openweathermap api"""
-    response = urllib2.urlopen('http://api.openweathermap.org/data/2.5/weather?q=Ostrava,cz&APPID=be5bc367823a074effb436947b75a397') #put your city ID number at the end
+    response = urllib2.urlopen('http://api.openweathermap.org/data/2.5/weather?q=Ostrava,cz&APPID=tvujklic') #put your city ID number at the end
     mydata = response.read()
     return mydata
  
